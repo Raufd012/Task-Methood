761ff543c36a0d6b49ea68b61547c67d184b9d01
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-03-19 02:29:13 +0400

    initial commit

---
+++ Task Methood/Task Methood/Program.cs
@@ -0,0 +1,156 @@
+﻿//task 1
+
+
+
+//namespace Methood_Tasks
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine(min());
+//        }
+//        public static int min()
+//        {
+//            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };
+//            int small = number[0];
+
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                if (arr[i] > small)
+//                {
+//                    small = arr[i];
+//                }
+
+
+//            }
+//            return small;
+//        }
+//    }
+//}
+
+
+
+//task 2.1
+
+
+
+
+//namespace Methood_Tasks
+//{
+//    internal class Program
+//    {
+//        public static void Main()
+//        {
+//            //S = p*r² (p=3)
+//            int p = 3;
+//            int r = 6;
+//            int Area = (p * r * r);
+//            Console.WriteLine($"{Area}");
+//            Console.ReadKey();
+//        }
+
+
+//    }
+//}
+
+
+
+// task 2.2
+
+
+
+
+
+//namespace Methood_Tasks
+//{
+//    internal class Program
+//    {
+
+
+
+
+//        public static void Main()
+//        {
+//            //S = a*b
+//            int a = 15;
+//            int b = 6;
+//            int Area = a * b;
+//            Console.WriteLine($" {a} * {b} = {Area}");
+//            Console.ReadKey();
+//        }
+
+
+//    }
+//}
+
+
+
+// task 2.3
+
+
+
+
+//namespace Methood_Tasks
+//{
+//    internal class Program
+//    {
+
+//        //S=2(a*b+a*c+b*c)
+
+
+//        public static void Main()
+//        {
+//            int a = 15;
+//            int b = 6;
+//            int c = 5;
+//            int Area = 2 * (a * b + a * c + b * c);
+//            Console.WriteLine($"{Area}");
+//            Console.ReadKey();
+//        }
+
+
+//    }
+//}
+
+
+
+
+//task 2.4
+
+
+
+
+
+namespace Methood_Tasks
+{
+    internal class Program
+    {
+
+        //S=p*r; p=(a+b+c)/2
+
+
+        public static void Main()
+        {
+            int a = 15;
+            int b = 6;
+            int c = 5;
+            int p = 10;
+            int r = 20;
+            int Area1 = p * r;
+            int Area2 = 2 / (a + b + c);
+            Console.WriteLine($"{Area1 * Area2}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
